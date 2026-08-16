ba7ceb420ab9d7a8ab06c2d85338d64a33225967
Florian Delage <floriandlg@icloud.com>
2026-08-16 03:38:12 +0200

    fix typo

--- home/florian/features/cli/zoxide/default.nix
+++ home/florian/features/cli/zoxide/default.nix
@@ -1,7 +1,7 @@
 {
     programs.zoxide = {
         enable = true;
-        enableFishIntegration = true;
+        enableZshIntegration = true;
         options = [
             "--cmd cd"
         ];
