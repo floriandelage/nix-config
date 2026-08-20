0e3a394698595913d481ed92d767c7522117a3b2
Florian Delage <floriandlg@icloud.com>
2026-08-20 00:35:59 +0000

    make ssh host key look for persistent one

--- hosts/common/global/sops.nix
+++ hosts/common/global/sops.nix
@@ -13,7 +13,7 @@ in {
         validateSopsFiles = false;
 
         age = {
-            sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
+            sshKeyPaths = ["/persistent/etc/ssh/ssh_host_ed25519_key"];
             keyFile = "/var/lib/sops-nix/key.txt";
             generateKey = true;
         };
