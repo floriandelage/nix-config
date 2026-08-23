c72f861707f54239bd346f9b9597a484815f2776
Florian Delage <floriandlg@icloud.com>
2026-08-23 21:27:23 +0200

    add github to knownHosts

--- hosts/common/global/openssh.nix
+++ hosts/common/global/openssh.nix
@@ -28,13 +28,19 @@ in {
         ];
     };
 
-    programs.ssh.knownHosts = lib.mapAttrs
-    (hostname: publicKeyFile: {
-        inherit publicKeyFile;
+    programs.ssh.knownHosts =
+        lib.mapAttrs
+        (hostname: publicKeyFile: {
+            inherit publicKeyFile;
 
-        extraHostNames = lib.optional
-        (hostname == config.networking.hostName)
-        "localhost";
-    })
-    hostKeyFiles;
+            extraHostNames = lib.optional
+            (hostname == config.networking.hostName)
+            "localhost";
+        })
+        hostKeyFiles
+        // {
+            "github.com" = {
+                publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
+            };
+        };
 }
